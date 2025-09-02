7f956818eaebfb5d891d1cf7b5b5de121019c7e9
bernotieno <bernadokumu1@gmail.com>
2025-09-02 11:49:06 +0300

    feat: import PopularResidencesSection component into Home for display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/header"
 import HeroSection from "./components/hero"
 import RealEstateSection from "./components/statisctics"
 import WhyChooseUsSection from "./components/whychooseus"
+import PopularResidencesSection from "./components/popularsection"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <RealEstateSection />
       <WhyChooseUsSection />
+      <PopularResidencesSection />
     </div>
   )
 }
