64028e39d7e0397f96cc6cabddc4265c92bdd5b8
bernotieno <bernadokumu1@gmail.com>
2025-09-02 11:26:29 +0300

    feat: import RealEstateSection component into Home for statistics display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/header"
 import HeroSection from "./components/hero"
+import RealEstateSection from "./components/statisctics"
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
       <HeroSection />
+      <RealEstateSection />
     </div>
   )
 }
